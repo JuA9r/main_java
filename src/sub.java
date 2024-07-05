ec3c982562f4fddace0b26b7dea65c9149cf1ab9
JuA9r <jiujingz05@gmail.com>
2024-07-05 09:06:45 +0900

    Changes

--- src/sub.java
+++ src/sub.java
@@ -2,24 +2,34 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+
 public class sub {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Sample");
-        JButton button = new JButton("A");
-        
-        button.setBounds(150, 150, 100, 100);
-        button.addActionListener(new ButtonAction());
-
-        frame.add(button);
         frame.setSize(400, 400);
         frame.setLayout(null);
-        frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        for (int i = 1; i <= 9; i++) {
+            JButton button = new JButton(String.valueOf(i));
+            button.setBounds((i-1)%3*100+50, (i-1)/3*100+50, 80, 80);
+            button.addActionListener(new ButtonAction(i));
+            frame.add(button);
+        }
+
+        frame.setVisible(true);
     }
 }
 
 class ButtonAction implements ActionListener {
+    private int number;
+
+    public ButtonAction(int number) {
+        this.number = number;
+    }
+
+    @Override
     public void actionPerformed(ActionEvent e) {
-        System.out.println("Button clicked!");
+        System.out.println("Button Pressed: " + number);
     }
 }
