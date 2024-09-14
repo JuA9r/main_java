98cbf1e84b6dd14b5725977dcc877610493544be
JuA9r <jiujingz05@gmail.com>
2024-09-14 16:59:01 +0900

    Changes

--- src/calculator.java
+++ src/calculator.java
@@ -8,23 +8,32 @@ class calculator extends JFrame {
         frame.setVisible(true);
     }
 
+    // generate window
     calculator(String title){
         setTitle(title);
         setBounds(100, 100, 320, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null);
 
-        JPanel panel = new JPanel();
-
+        // generate textbox and input character
         JTextField text = new JTextField();
         text.setForeground(Color.GREEN);
         text.setBackground(Color.BLACK);
         text.setCaretColor(Color.GREEN);
         text.setFont(new Font("Meiryo", Font.ITALIC, 20));
-        text.setColumns(15);
+        text.setBounds(10, 10, 280, 40);
+        add(text);
 
-        panel.add(text);
+        // set button size
+        int buttonSize = 70;
 
-        Container contentPanel = getContentPane();
-        contentPanel.add(panel, BorderLayout.CENTER);
+        // generate button
+        for (int i=1; i<=9; i++) {
+            JButton button = new JButton(String.valueOf(i));
+            int x = 10+((i-1)%3)*(buttonSize+1);
+            int y = 60+((i-1)/3)*(buttonSize+1);
+            button.setBounds(x, y, buttonSize, buttonSize);
+            add(button);
+        }
     }
 }
